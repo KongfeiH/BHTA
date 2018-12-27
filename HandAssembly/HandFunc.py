ce988cfc0332b06b9932923be8c2335a6e570be7
KongfeiH <40188526+KongfeiH@users.noreply.github.com>
2018-12-27 21:49:15 +0800

    add some class

--- HandAssembly/HandFunc.py
+++ HandAssembly/HandFunc.py
@@ -34,6 +34,16 @@ FINGER2 = 12	# Puck ID for F2
 FINGER3 = 13	# Puck ID for F3
 SPREAD  = 14	# Puck ID for SP
 HAND_GROUP = 0x405 	# Refers to all motors that respond to group ID 5.
+
+
+
+MODE_VEL = 4
+V = 44
+TSTOP = 78
+
+
+
+
 class Hand():
     def __init__(self):
 
@@ -234,3 +244,57 @@ class SensorShow(HandSensor):
 
             plt.ioff()
             self.get_full_tact()
+    def DynamicData(self):
+        dataTest=[]
+        plt.ion()
+        for i in range(10000):
+           dataTest.append(self.finger1)
+           plt.clf()
+           plt.imshow(dataTest)
+           plt.pause(0.33)
+           plt.ioff()
+           self.get_full_tact()
+
+class VelocityMoveControl():
+    def __init__(self):
+        # Because fingers and spread may collide, we do not want the hand to move
+        # spread at the same time as the fingers.
+        hand.set_property(SPREAD, V, 0)
+        hand.set_property(FINGER1, V, 200)  # Units are encoder counts per second
+        hand.set_property(FINGER2, V, 200)
+        hand.set_property(FINGER3, V, 200)
+
+        # Set the timestop property to wait an arbitrarily long amount of time.
+        # In general, the argument to TSTOP is amount of time in miliseconds that the
+        # hand will wait to finish moving. Setting TSTOP to 0, however, makes that
+        # tolerance level infinite. By default, the fingers wait 50 ms, and the spread
+        # waits up to 150 ms.
+        hand.set_property(HAND_GROUP, TSTOP, 0)
+
+        # The hand must now go into velocity mode to move.
+        hand.set_property(HAND_GROUP, MODE, MODE_VEL)
+        time.sleep(1.5)
+        hand.set_property(HAND_GROUP, MODE, MODE_IDLE)
+
+        # Using the exact same process, we close the fingers.
+        hand.set_property(FINGER1, V, -200)
+        hand.set_property(FINGER2, V, -200)
+        hand.set_property(FINGER3, V, -200)
+        hand.set_property(HAND_GROUP, MODE, MODE_VEL)
+        time.sleep(1.5)
+        hand.set_property(HAND_GROUP, MODE, MODE_IDLE)
+
+        # Now, we open and close the spread.
+        hand.set_property(HAND_GROUP, V, 0)
+        hand.set_property(SPREAD, V, 60)
+        hand.set_property(SPREAD, MODE, MODE_VEL)
+        time.sleep(1.5)
+        hand.set_property(SPREAD, MODE, MODE_IDLE)
+        hand.set_property(SPREAD, V, -60)
+        hand.set_property(SPREAD, MODE, MODE_VEL)
+        time.sleep(1.5)
+
+        # Because having an infinite timestop can wear out the hand, we now return them
+        # to their default values.
+        hand.set_property(HAND_GROUP, TSTOP, 50)
+        hand.set_property(SPREAD, TSTOP, 150)
